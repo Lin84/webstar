28d6ae7b719d05f35a55ac5aed86a901cdef1d23
Linh Le Tuan <linh.tuan@actum.cz>
2017-08-26 14:31:15 +0200

    add sass-loader and node-sass

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
 // import css from './../styles/styles.css';
-const css = require('./../styles/styles.css')
+const css = require('./../styles/styles.scss')
 
 console.log('add style-loader and css-loader');
