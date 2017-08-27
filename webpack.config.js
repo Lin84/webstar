43fa07833fe080b3cdd59101f1b9f0d99ee26867
Linh Le Tuan <linh.tuan@actum.cz>
2017-08-27 20:15:34 +0200

    how to load images through css or template with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
             {
                 test: /\.pug$/,
                 use: ['html-loader', 'pug-html-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: 'file-loader'
             }
         ]
     },
