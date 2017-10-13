adb41fc9988e79327592b56dedfa3840ef9a4eb0
Linh Le Tuan <linh.tuan@actum.cz>
2017-10-13 11:24:04 +0200

    add redux

--- src/scrips/main.js
+++ src/scrips/main.js
@@ -1,10 +1,21 @@
+/**
+ * main styles:
+ */
 import css from './../styles/main.scss';
-import { render } from './ultilities/render';
+
+/**
+ * React Component:
+ */
+import configureStore from './redux/store/configureStore';
+import { render, renderFactory } from './ultilities/render';
 import Headline from './components/_common/Headline';
+import PlusOne from './components/plus-one/PlusOne';
 
 const app = (config) => {
-    // const store = configureStore(config);
+    const store = configureStore(config);
+
     render(Headline, document.querySelector('#root'));
+    renderFactory(PlusOne, document.querySelectorAll('.plus-one'), {}, store);
 };
 
 app(window.config);
---
+++ src/scrips/redux/store/configureStore.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './root-reducer';
+// import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const enhancer = applyMiddleware(
+    thunk
+);
+
+export default function configureStore(initialState) {
+    /* eslint-disable no-underscore-dangle */
+    return createStore(
+        rootReducer,
+        initialState,
+        compose(
+            enhancer,
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
+    );
+    /* eslint-enable */
+}
