76b9439c1a14103cd643ceae6ec020e84a321880
Linh Le Tuan <linh.tuan@actum.cz>
2017-11-05 17:13:28 +0100

    add comments

--- src/scrips/main.js
+++ src/scrips/main.js
@@ -13,15 +13,26 @@ import PlusOne from './components/plus-one/PlusOne';
 import Link from './components/_common/Link';
 import Button from './components/_common/Button';
 
+/**
+ * @param {object} config
+ */
 const app = config => {
     const store = configureStore(config);
 
+    // Demo render react component once:
     render(Headline, document.querySelector('#headline'), { text: 'hello' });
+
+    // Demo render react component more times:
     renderFactory(Link, document.querySelectorAll('.link'));
+
+     // Demo render react component connected to the Redux store:
     renderFactory(PlusOne, document.querySelectorAll('.plus-one'), {}, store);
+
+     // Demo testing react component with jest and enzyme:
     renderFactory(Button, document.querySelectorAll('.button'), {
         class: 'btn btn-primary',
-        label: 'Submit'
+        label: 'Submit',
+        handleClick: () => { console.log('Click') }
     });
 };
 
